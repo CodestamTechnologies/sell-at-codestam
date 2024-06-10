4d2d6119603a3abebe40d821282f055152524509
Harshit <sharmaharshitraj@gmail.com>
2024-06-10 14:03:06 +0530

    added maxwidthwrapper

---
+++ src/components/MaxWidthWrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; // Import React
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}) => {
+  return (
+    <div
+      className={`mx-auto w-full max-w-screen-xl px-2.5 md:px-20 ${className}`}>
+      {children}
+    </div>
+  );
+}
+
+export default MaxWidthWrapper;
